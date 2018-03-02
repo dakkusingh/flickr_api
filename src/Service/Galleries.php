464728824abd7e8ae80e07f2b8ea09c7638d8283
dakku <dakku@97634.no-reply.drupal.org>
2018-03-02 18:32:05 +0000

    Adding galleries methods

--- src/Service/Galleries.php
+++ src/Service/Galleries.php
@@ -26,4 +26,94 @@ class Galleries {
     $this->client = $client;
   }
 
+  /**
+   * Returns info about a given gallery.
+   *
+   * @param string $id
+   *   NSID of the gallery whose photos you want.
+   *
+   * @return array $response
+   *   Response from the flickr method flickr.gallery.getInfo.
+   *   (https://www.flickr.com/services/api/flickr.gallery.getInfo.html)
+   */
+  function galleriesGetInfo($id, $other_args = []) {
+    $args = ['gallery_id' => $id];
+    array_merge($args, $other_args);
+
+    $response = $this->client->request(
+      'flickr.galleries.getInfo',
+      $args,
+      FALSE
+    );
+
+    if ($response) {
+      return $response['gallery'];
+    }
+
+    return FALSE;
+  }
+
+  /**
+   * Returns a list of photos for a given gallery.
+   *
+   * @param string $id
+   *   ID of the gallery.
+   *
+   * @return array $response
+   *   Response from the flickr method flickr.galleries.getPhotos.
+   *   (https://www.flickr.com/services/api/flickr.galleries.getPhotos.html)
+   */
+  function galleriesGetPhotos($id, $page = 1, $other_args = []) {
+    $args = [
+      'gallery_id' => $id,
+      'page' => $page,
+    ];
+    array_merge($args, $other_args);
+
+    // Set per_page to flickr module default if not specified in $args.
+    if (!isset($args['per_page'])) {
+      // TODO Expose pager as a setting.
+      $args['per_page'] = 6;
+    }
+
+    $response = $this->client->request(
+      'flickr.galleries.getPhotos',
+      $args
+    );
+
+    if ($response) {
+      return $response['photos'];
+    }
+
+    return FALSE;
+  }
+
+  /**
+   * Returns the galleries curated by the specified user.
+   *
+   * @param string $nsid
+   *   NSID of the user whose photoset list you want.
+   *
+   * @return array
+   *   Response from the flickr method flickr.galleries.getList.
+   *   (https://www.flickr.com/services/api/flickr.galleries.getList.html)
+   */
+  function galleriesGetList($nsid, $page = 1) {
+    $args = [
+      'user_id' => $nsid,
+      'page' => $page,
+    ];
+
+    $response = $this->client->request(
+      'flickr.galleries.getList',
+      $args
+    );
+
+    if ($response) {
+      return $response['galleries']['gallery'];
+    }
+
+    return FALSE;
+  }
+
 }
