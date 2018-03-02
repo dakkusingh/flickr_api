94b8b261ab8fd0e02015b7c5a3420b6935e87f1c
dakku <dakku@97634.no-reply.drupal.org>
2018-03-02 21:31:35 +0000

    method to get favs

--- src/Service/Favorites.php
+++ src/Service/Favorites.php
@@ -20,4 +20,42 @@ class Favorites {
     $this->client = $client;
   }
 
+  /**
+   * Returns a list of favorite public photos for the given user.
+   *
+   * @param string $nsid
+   *   NSID of the user whose photoset tags will be returned.
+   * @param string $page
+   *   Page of results to return.
+   *
+   * @return array
+   *   Response from the flickr method flickr.favorites.getPublicList.
+   *   (https://www.flickr.com/services/api/flickr.favorites.getPublicList.html)
+   */
+  function favoritesGetPublicList($nsid, $page = 1, $other_args = []) {
+    $args = [
+      'user_id' => $nsid,
+      'page' => $page,
+    ];
+
+    array_merge($args, $other_args);
+
+    // Set per_page to flickr module default if not specified in $args.
+    if (!isset($args['per_page'])) {
+      // TODO Expose pager as a setting.
+      $args['per_page'] = 6;
+    }
+
+    $response = $this->client->request(
+      'flickr.favorites.getPublicList',
+      $args
+    );
+
+    if ($response) {
+      return $response['photos'];
+    }
+
+    return FALSE;
+  }
+
 }
