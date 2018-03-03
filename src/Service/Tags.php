7eb19d7495f406d161ec433697bffcc295ae63ea
dakku <dakku@97634.no-reply.drupal.org>
2018-03-03 00:48:39 +0000

    Adding service to lookup tags

--- src/Service/Tags.php
+++ src/Service/Tags.php
@@ -20,4 +20,62 @@ class Tags {
     $this->client = $client;
   }
 
+  /**
+   * Get the popular tags for a given user.
+   *
+   * @param string $nsid
+   *   NSID of the user whose tags will be returned.
+   * @param string $count
+   *   Number of tags to return.
+   *
+   * @return array
+   *   Response from the flickr method flickr.tags.getListUserPopular.
+   *   (https://www.flickr.com/services/api/flickr.tags.getListUserPopular.html)
+   */
+  public function tagsGetListUserPopular($nsid, $count = NULL) {
+    $args = ['user_id' => $nsid];
+
+    if ($count != NULL) {
+      $args['count'] = $count;
+    }
+
+    $response = $this->client->request(
+      'flickr.tags.getListUserPopular',
+      $args
+    );
+
+    if ($response) {
+      return $response['who']['tags']['tag'];
+    }
+
+    return FALSE;
+  }
+
+  /**
+   * Get the tag list for a given user.
+   *
+   * @param string $nsid
+   *   NSID of the user whose photoset tags will be returned.
+   *
+   * @return array
+   *   Response from the flickr method flickr.tags.getListUser.
+   *   (https://www.flickr.com/services/api/flickr.tags.getListUser.html)
+   */
+  public function tagsGetListUser($nsid) {
+    $args = [
+      'user_id' => $nsid,
+    ];
+
+    $response = $this->client->request(
+      'flickr.tags.getListUser',
+      $args
+    );
+
+    if ($response) {
+      return $response['who']['tags']['tag'];
+    }
+
+    return FALSE;
+  }
+
 }
