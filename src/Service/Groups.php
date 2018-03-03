796ebcc79403f3bf532b9c069bad1025572458cb
dakku <dakku@97634.no-reply.drupal.org>
2018-03-03 00:47:11 +0000

    Service to lookup group

--- src/Service/Groups.php
+++ src/Service/Groups.php
@@ -15,9 +15,44 @@ class Groups {
   /**
    * Constructor for the Flickr API Groups class.
    */
-  public function __construct(Client $client) {
+  public function __construct(Client $client, Helpers $helpers) {
     // Flickr API Client.
     $this->client = $client;
+
+    // Flickr API Helpers.
+    $this->helpers = $helpers;
+  }
+
+  /**
+   * Returns info about a given group.
+   *
+   * @param string $id
+   *   NSID of the group whose photos you want.
+   *
+   * @return array
+   *   Response from the flickr method flickr.groups.getInfo.
+   *   (https://www.flickr.com/services/api/flickr.groups.getInfo.html)
+   */
+  public function groupsGetInfo($id, $other_args = []) {
+    if ($this->helpers->isNsid($id)) {
+      $args = ['group_id' => $id];
+    }
+    else {
+      $args = ['group_path_alias' => $id];
+    }
+
+    array_merge($args, $other_args);
+
+    $response = $this->client->request(
+      'flickr.groups.getInfo',
+      $args
+    );
+
+    if ($response) {
+      return $response['group'];
+    }
+
+    return FALSE;
   }
 
 }
